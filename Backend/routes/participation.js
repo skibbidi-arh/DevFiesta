54767db36a67d8bc9d7dc27dc04732f719d82eb9
ironicalcrow <arhamkhan@iut-dhaka.edu>
2025-08-16 11:28:03 +0600

    ebar hoyto route jabe

---
+++ Backend/routes/participation.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const participantController = require("../controllers/participantController");
+const { verifyToken } = require("../middleware/auth");
+
+router.post("/create", verifyToken, participantController.team_creation);
+router.get("/hackathon/:hackathon_id", verifyToken, participantController.get_teams);
+router.get("/members/:team_id", verifyToken, participantController.get_team_members);
+router.post("/mark", verifyToken, participantController.marking_teams);
+router.get("/leaderboard/:hackathon_id", verifyToken, participantController.leader_board);
+
+module.exports = router;
